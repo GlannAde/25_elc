578b42c2475bd69a741a7c440b78e8039acfdd73
GlennAde <1519623572@qq.com>
2026-04-07 13:09:48 +0800

    fix: Update permissions commands and correct script path in start.sh

--- src/start.sh
+++ src/start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-echo "520" | sudo -S chmod 777 /home/elc/src/start.sh
-echo "520" | sudo -S chmod 777 /home/elc/src/main.py
+echo ";'" | sudo -S chmod 777 /home/elc/src/start.sh
+echo ";'" | sudo -S chmod 777 /home/elc/src/main.py
 v4l2-ctl --list-devices  # 列出所有视频设备
 # sudo rdk-miniboot-update
-/usr/bin/python3 /home/kielas/elc/src/main.py
+/usr/bin/python3 /home/opi5max/elc/src/main.py
